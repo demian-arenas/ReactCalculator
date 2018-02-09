653fd13cee76d8c67930689cc022be0bc856dac2
Demian Arenas <arenasdemian99@gmail.com>
2018-02-09 05:33:25 -0600

    [update] created Calculator layout

--- src/ReactCalculator.js
+++ src/ReactCalculator.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
-import { Text, AppRegistry } from "react-native";
+import { View, Text, AppRegistry } from "react-native";
 
 class ReactCalculator extends Component {
   render() {
-    return <Text>Hello, React!</Text>;
+    return (
+        <View style={{flex: 1}}>
+            <View style={{flex: 2, backgroundColor: '#193441'}}></View>
+            <View style={{flex: 8, backgroundColor: '#3E606F'}}></View>
+        </View>
+    )
   }
 }
 

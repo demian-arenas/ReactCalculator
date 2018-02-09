e7e3549ad75915b0b4e92bf7ca5957c40ce36524
Demian Arenas <arenasdemian99@gmail.com>
2018-02-09 05:27:32 -0600

    Hello world React Native

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import React, { Component } from "react";
+import { Text, AppRegistry } from "react-native";
 
-AppRegistry.registerComponent('ReactCalculator', () => App);
+class ReactCalculator extends Component {
+  render() {
+    return <Text>Hello, React!</Text>;
+  }
+}
+
+AppRegistry.registerComponent("ReactCalculator", () => ReactCalculator);

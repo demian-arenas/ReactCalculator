0f6a82f7a4b70a1d73dcfff6d15efa932ece0258
Demian Arenas <arenasdemian99@gmail.com>
2018-02-09 05:29:19 -0600

    [add] reorganize project

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-import React, { Component } from "react";
-import { Text, AppRegistry } from "react-native";
-
-class ReactCalculator extends Component {
-  render() {
-    return <Text>Hello, React!</Text>;
-  }
-}
-
-AppRegistry.registerComponent("ReactCalculator", () => ReactCalculator);
+import ReactCalculator from './src/ReactCalculator';
